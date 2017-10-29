d3d26ec3af73f80bdd5f6a8bc85b3d2702ec24d9
Jekyll Song <songjiuwu@gmail.com>
2017-10-29 16:27:52 +0800

    Recommandation#1

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/database/RecommendationDBEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/database/RecommendationDBEngine.java
@@ -1,13 +1,55 @@
 package com.example.bot.spring.database;
 
-import com.linecorp.bot.model.event.message.TextMessageContent;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+//import com.linecorp.bot.model.event.message.TextMessageContent;
 
 public class RecommendationDBEngine extends DBEngine {
 	public RecommendationDBEngine() {
 		
 	}
 	
-	public int recommendationQuery(TextMessageContent text) {
-		return 0;
+	public String recommendationQuery(String userId, ArrayList<String> text) {
+		Connection connection = getConnection();
+		//insert into the unanswered question table to store the question
+		String response="";
+		String idList="";
+		for(int i=0; i<text.length; i++) {
+			try {
+				ResultSet rs = null;
+				PreparedStatement stmt = connection.prepareStatement(
+					"select * from tour_features where "+text.get(i))+"=1";
+				String temp="Tours with good "+text.get(i)+" are:"
+			
+				rs=stmt.executeQuery()
+				if (rs==null) {
+					response="No tours with good "+text.get(i)+"\n";
+				}
+				else {
+					while (rs.next()) {	
+						String tourid = rs.getString(1);
+						temp+=tourid+", ";
+						idList+=tourid+", "
+					}
+					temp=temp.replaceAll(", $", "");
+					response+=temp+"\n";
+				}
+				tourid=tourid.replaceAll(", $", "");
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		stmt=connection.prepareStatement(
+				"update line_user_info set TourIDs ='"+idList+"'where UserID="+userID+";");
+		stmt.executeUpdate();
+
+		stmt.close();
+		connection.close();
+		
+		return response;
 	}
 }
