581cb24a0e58ce7fadbbcfefba646425d5d3a4b9
yuanyuan <yuanyuanqi777@gmail.com>
2017-10-30 20:03:35 +0800

    first try on TextProcessor

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/TextProcessor.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/TextProcessor.java
@@ -4,31 +4,76 @@ import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.example.bot.spring.textsender.*;
 
 public class TextProcessor {
-	
+
+	DBEngine DBE;
 	public TextProcessor() {
 		// TODO Auto-generated constructor stub
+		DBE=new DBEngine();
 	}
 	
-	public String processText(String userId, String text) {
-		
-		// update line_user_info table; 
-		
-		// categorize input message (ANOTHER DB REQUIRED)
-		
-		// SQ Test Sender; 
-		String reply = null;  // = sq test sender;
-		SQTextSender sqsender = new SQTextSender();
-       try {
-    	   reply = sqsender.process(userId, text);
+	private String classifyText(String userId, String text)throws Exception {
+		String reply="";
+		String tag;
+		try {
+			SQTextSender sqsender = new SQTextSender();
+			reply = sqsender.process(userId, text)+"\n";
+		}catch(Exception e) {}
+		try {	
+			if(text.toLowerCase().contains("recommend")||
+			   text.toLowerCase().contains("do you have")) {
+				RecommendationTextSender rsender = new RecommendationTextSender();
+				reply += rsender.process(userId, text);
+				DBE.update(userId,"categoriztion","reco");
+				return reply;
+			}
+
+			if(text.toLowerCase().contains("tell me")) {
+				GQTextSender gqsender = new GQTextSender;
+				reply += gqsender.process(userId, text);
+				DBE.update(userId,"categoriztion", "gq");
+				return reply;
+			}
+
+			if(text.toLowerCase().contains("book")) {
+				BookingTextSender bsender = new BookingTextSender();
+				reply += bsender.process(userId, text);
+				DBE.update(userId,"categoriztion","book");
+				return reply;
+			}
+			
+			tag=DBE.get(userId,"categoriztion");
+			switch(tag) {
+			case "reco":
+				RecommendationTextSender rsender = new RecommendationTextSender();
+				reply += rsender.process(userId, text);
+				break;
+			case "book":
+				BookingTextSender bsender = new BookingTextSender();
+				reply += bsender.process(userId, text);
+				break;
+			case "gq":
+				GQTextSender gqsender = new GQTextSender;
+				reply += gqsender.process(userId, text);
+				break;
+			default:
+				reply += "Could you please specify that you want some recommendation, asking some general question, or booking a trip";
+			}
+			return reply;
 			
 		} catch (Exception e) {
-			System.out.println("---------- inside handleTextContent ---------- ");
-			System.err.println(e.getMessage());
-		}		
-		
-		
-		
-				
-		return reply;
+			// TODO: call unanswered question
+		}
+		throw Exception("Still not return");
+		return null;
+
+	}
+
+	public String processText(String userId, String text) throws Exception{
+		if (text=null) {
+			throw Exception("no input");
+		}
+		DBE.update(userId,"lastq",text);
+		String reply=classifyText(userID,text);
+		DBE.update(userId,"lasta",reply);
 	}
 }
