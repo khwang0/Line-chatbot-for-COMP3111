e225e785680ce5db15fc9d05a0dbe3b79608b513
zysjimmy <zhouyusheng.jimmy@gmail.com>
2017-11-02 23:43:52 +0800

    Test #5

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/CustomerController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/CustomerController.java
@@ -3,12 +3,13 @@ package com.example.bot.spring;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CustomerController {
 	
-    @RequestMapping("/customers")
+    @RequestMapping(value = "/customers", method = RequestMethod.GET)
     public String getCustomers(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "greeting";
