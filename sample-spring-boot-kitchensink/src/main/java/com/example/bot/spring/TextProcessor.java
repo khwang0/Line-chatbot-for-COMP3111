7b51833a599f14e7c36605b1cf0bbc232c4ac22c
Crysitna <31640436+Crysitna@users.noreply.github.com>
2017-11-03 19:59:17 +0800

    ==

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/TextProcessor.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/TextProcessor.java
@@ -33,7 +33,7 @@ public class TextProcessor {
 					&& (label == null || label == "" || label == "default" || label == "none")){
 				//reply = "tag: " + tag + " label: " + label;
 				SQTextSender sqsender = new SQTextSender();
-				reply == sqsender.process(userId, text)+"\n";	
+				reply = sqsender.process(userId, text)+"\n";	
 			}
 			
 			if(tag.equals("book")) {
