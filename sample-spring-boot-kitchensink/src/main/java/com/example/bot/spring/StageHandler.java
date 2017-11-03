0cf276633a7e766bb99d135c326248d4c4e48282
King <xhuangat@connect.ust.hk>
2017-11-03 23:41:18 +0800

    may be last bugv2

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/StageHandler.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/StageHandler.java
@@ -119,7 +119,7 @@ public class StageHandler {
 		case 5:{
 			if(inputChecker.AgeEditting(text, currentUser, database, "set")) {
        			replymsg="Your data has been recorded.\nInput anything to conitnue.";
-				database.updateUser(currentUser);
+				database.pushUser(currentUser);
        			currentUser.setStage("Main");
        			currentUser.setSubStage(0);
 			}
