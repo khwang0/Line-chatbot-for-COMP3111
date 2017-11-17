0496e699639c49aaabe1c07fa824b417b9483352
zysjimmy <zhouyusheng.jimmy@gmail.com>
2017-11-17 11:52:17 +0800

    Test again

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/database/WebAppDBEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/database/WebAppDBEngine.java
@@ -58,7 +58,7 @@ public class WebAppDBEngine extends DBEngine {
 		LinkedList<Tour> allTours = new LinkedList<Tour>();
 		PreparedStatement nstmt;
 			nstmt = connection.prepareStatement(
-					"SELECT  b.bootableid, i.tour_name, b.tourdate, b.tourguideid, b.hotel, b.tourcapacity, b.registerednum"
+					"SELECT  b.bootableid, i.tour_name, b.tourdate, b.tourguideid, b.hotel, b.tourcapcity, b.registerednum"
 					+ " FROM booking_table b, tour_touroffer_relation o, tour_info i "
 					+ " WHERE b.bootableid = o.bootableid"
 					+ " AND i.tourid = o.tourid");
