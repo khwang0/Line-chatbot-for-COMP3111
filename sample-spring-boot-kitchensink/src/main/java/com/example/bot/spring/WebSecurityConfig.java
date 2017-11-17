c83a059e47a572816c4cde364e9a37e86e5cb7c6
zysjimmy <zhouyusheng.jimmy@gmail.com>
2017-11-17 13:08:13 +0800

    Test again

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/WebSecurityConfig.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/WebSecurityConfig.java
@@ -1,21 +1,21 @@
 package com.example.bot.spring;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	/**
 	 *  defines which URL paths should be secured and which should not
 	 *  Specifically, the "/" and "/home" paths are configured to not require any authentication. All other paths must be authenticated.
 	 * */
     
-    //@Override
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
         .authorizeRequests()
