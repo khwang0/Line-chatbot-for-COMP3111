aa3caabb640a60971f25af764b515c4cfda8e615
zysjimmy <zhouyusheng.jimmy@gmail.com>
2017-11-17 12:58:02 +0800

    Test again

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/WebSecurityConfig.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/WebSecurityConfig.java
@@ -1,10 +1,17 @@
 package com.example.bot.spring;
 
+
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -13,6 +20,7 @@ public class WebSecurityConfig {
 	 *  defines which URL paths should be secured and which should not
 	 *  Specifically, the "/" and "/home" paths are configured to not require any authentication. All other paths must be authenticated.
 	 * */
+    
     //@Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -26,6 +34,24 @@ public class WebSecurityConfig {
             .and()
         .logout()
             .permitAll();
+        http.csrf().requireCsrfProtectionMatcher(new RequestMatcher() {
+            private Pattern allowedMethods = Pattern.compile("^(GET|HEAD|TRACE|OPTIONS)$");
+            private RegexRequestMatcher apiMatcher = new RegexRequestMatcher("/v[0-9]*/.*", null);
+
+            @Override
+            public boolean matches(HttpServletRequest request) {
+                // No CSRF due to allowedMethod
+                if(allowedMethods.matcher(request.getMethod()).matches())
+                    return false;
+
+                // No CSRF due to api call
+                if(apiMatcher.matches(request))
+                    return false;
+
+                // CSRF for everything else that is not an API call or an allowedMethod
+                return true;
+            }
+        });
     /* .authorizeRequests()
      * 		.anyRequest().authenticated() -- every request requires the user to be authenticated
      * .formLogin()						  -- form based authentication is supported
