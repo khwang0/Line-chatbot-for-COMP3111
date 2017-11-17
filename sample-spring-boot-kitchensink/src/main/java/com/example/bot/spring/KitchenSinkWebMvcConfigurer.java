ea158b0caf9aa7c760c72a363a7ca7e80ab3acff
zysjimmy <zhouyusheng.jimmy@gmail.com>
2017-11-17 13:39:14 +0800

    Test again

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkWebMvcConfigurer.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkWebMvcConfigurer.java
@@ -40,7 +40,6 @@ public class KitchenSinkWebMvcConfigurer extends WebMvcConfigurerAdapter {
     
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("home");
         registry.addViewController("/login").setViewName("login");
     }
 }
