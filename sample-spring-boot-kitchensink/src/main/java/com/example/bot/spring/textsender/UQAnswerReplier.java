a6747affb990fec336afa2e4e3667263a2b135eb
Jekyll Song <songjiuwu@gmail.com>
2017-11-18 20:14:09 +0800

    unanswered_question sender

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/textsender/UQAnswerReplier.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/textsender/UQAnswerReplier.java
@@ -1,12 +1,32 @@
 package com.example.bot.spring.textsender;
 
-public class UQAnswerReplier{
+import java.util.*
+//import java.util.GregorianCalendar;
+//import java.util.List;
+//import java.util.Set;
+//import java.util.concurrent.TimeUnit;
+import com.example.bot.spring.database.*;
+
+public class UQAnswerReplier implements Broadcaster{
 
 	public UQAnswerReplier() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public String replyUnansweredQuestion(String msg, String id) {
-		return null;
+	public int replyUnansweredQuestion(String msg, String id) {
+		UQDBEngine searchEngine = new UQDBEngine();
+		ArrayList<String> reply=searchEngine.answer();
+		for(int i=0; i<reply.size(); i++) {
+			String userID;
+			String question;
+			String answer;
+			String[3] temp = reply.split(",");
+			userID=temp[0];
+			question=temp[1];
+			answer=temp[2];
+			Message message = new TextMessage("For your question "+question+", the answer is "+answer);
+			lineMessagingClient.pushMessage(new PushMessage(userID, message));
+		}
+		return 0;
 	}
 }
