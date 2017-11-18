a9a7bda103b3d76cfb68e251e1bd1c05e6365bc8
Jekyll Song <songjiuwu@gmail.com>
2017-11-18 19:25:32 +0800

    exception handler changed

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/database/UQDBEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/database/UQDBEngine.java
@@ -13,68 +13,40 @@ public class UQDBEngine extends DBEngine {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public String retrieveReply(){
+	public String retrieveReply() throws Exception {
 		Connection connection = null;
 		PreparedStatement stmt = null;
 		String reply="";
-		try {
-			connection=getConnection();
-			stmt = connection.prepareStatement("select * from unanswered_default_reply");
-			ResultSet rs=stmt.executeQuery();
-			while (rs.next()) {
-				reply=rs.getString(1);
-			}
-		}catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
-			try {
-				if (stmt != null) stmt.close();
-				if (connection != null) connection.close();
-			} catch (Exception e2) {
-				System.err.println(e2.getMessage());
-			}
+
+		connection=getConnection();
+		stmt = connection.prepareStatement("select * from unanswered_default_reply");
+		ResultSet rs=stmt.executeQuery();
+		while (rs.next()) {
+			reply=rs.getString(1);
 		}
-		if (reply.equals("")) {
-			return "Database fatal error";
-		}
-		else {
-			return reply;
-		}
+		
+		if (stmt != null) stmt.close();
+		if (connection != null) connection.close();
+
+		return reply;
 	}
 	
-	public String uqQuery(String userId, String text) {
+	public String uqQuery(String userId, String text) throws Exception{
 		//System.out.println("Success");
 		Connection connection = null;
 		PreparedStatement stmt = null;
 		String reply="";
-		try {
-			connection = getConnection();
-			//insert into the unanswered question table to store the question
-			stmt = connection.prepareStatement(
-					"insert into unanswered_question values( '"+userId+"', '"+text+"', false, , false)"
-			);
-			stmt.executeUpdate();
-		} catch (URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
-			//
-			//stmt.close();
-			//connection.close();
-			try {
-				if (stmt != null) stmt.close();
-				if (connection != null) connection.close();
-			} catch (Exception e2) {
-				System.err.println(e2.getMessage());
-			}
-		}
+
+		connection = getConnection();
+		//insert into the unanswered question table to store the question
+		stmt = connection.prepareStatement(
+				"insert into unanswered_question values( '"+userId+"', '"+text+"', false, , false)"
+		);
+		stmt.executeUpdate();
+
+		if (stmt != null) stmt.close();
+		if (connection != null) connection.close();
+
 
 		return retrieveReply();
 	}
