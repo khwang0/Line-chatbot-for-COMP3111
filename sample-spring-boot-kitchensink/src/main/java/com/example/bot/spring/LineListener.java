2110307660211b804cb47f134bbd1e0b0c903830
yuanyuan qi <yuanyuanqi777@gmail.com>
2017-11-19 16:08:15 +0800

    hello

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/LineListener.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/LineListener.java
@@ -3,10 +3,12 @@ package com.example.bot.spring;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Component;
+=======
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+>>>>>>> be34857748acf9bf28ab523e48002190ebdd375e
 import com.example.bot.spring.textsender.*;
 
 @Component
