6a361b37522a4228a88996da7e6c52a8509178e9
QIYUANYUAN <yuanyuanqi777@gmail.com>
2017-11-19 16:01:01 +0800

    Update LineListener.java

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/LineListener.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/LineListener.java
@@ -3,12 +3,10 @@ package com.example.bot.spring;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-=======
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
->>>>>>> be34857748acf9bf28ab523e48002190ebdd375e
 import com.example.bot.spring.textsender.*;
 
 @Component
